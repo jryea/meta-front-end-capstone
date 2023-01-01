d12d07e2b3400ec441251416c88afed0c0cf996d
Jon ryea <jryea80@gmail.com>
2022-12-31 17:59:40 -0700

    CSS for homepage updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import './App.css';
+import React from 'react';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Main from './Components/Main';
 
 function App() {
-  return <div className='App'>Homepage</div>;
+  return (
+    <>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </>
+  );
 }
 
 export default App;
